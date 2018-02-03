aecacc3c1d54bc30db6927ca93860ddf4bc67273
Liu Cong <1434580150@qq.com>
2018-02-03 13:26:52 +0800

    add dev-sever add '--entry' '--output-filename'

--- src/js/script.js
+++ src/js/script.js
@@ -4,11 +4,11 @@ import {showButton,paragraph} from "./dom.js";
 var showPara = true;
 showButton.addEventListener("click",function(){
   if (showPara) {
-    showButton.textContent = "Show the paragraph!";
+    showButton.textContent = "Show the paragraph! Dynamicly!";
     paragraph.style.display = "none";
     showPara = false;
   }else{
-    showButton.textContent = "Hide the paragraph!";
+    showButton.textContent = "Hide the paragraph! Again! And again!";
     paragraph.style.display = "block";
     showPara = true;
   }
