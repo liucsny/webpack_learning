3ad12f7d9338618faa7bf8179e8a386fd83b023b
Liu Cong <1434580150@qq.com>
2018-02-03 15:11:44 +0800

    add css-loader style-loader

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./src/js/script.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    publicPath: "/dist"
+  },
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use:[
+          "style-loader",
+          "css-loader"    //excute "css-loader" first
+        ]
+      }
+    ]
+  }
+};
